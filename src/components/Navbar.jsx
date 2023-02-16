8c08a882493b408caedc33e7e70906a9fe39f048
herom <jerome.manset22@gmail.com>
2023-02-17 04:34:21 +0800

    add styles to the company lists

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,6 @@ const Navbar = (props) => {
     const [isFeaturesOpen, setIsFeaturesOpen] = useState(false)
     const [isCompanyOpen, setIsCompanyOpen] = useState(false)
 
-    
-
     function toggleList(e){
         const id = e.target.id
         if (id === "company"){
@@ -19,15 +17,25 @@ const Navbar = (props) => {
     <nav>
       <img className="icon--menu--close" src="./src/assets/images/icon-close-menu.svg" alt="X icon" onClick={props.handleClick}/>
       <ul className='nav--lists'>
-        <li id="features" className='nav--features' onClick={toggleList}>Features<span className={isFeaturesOpen ? 'icon--arrow--up' : 'icon--arrow--down'}></span></li>
+        <li id="features" 
+        className='nav--features' 
+        onClick={toggleList}>
+        Features
+        <span className={isFeaturesOpen ? 'icon--arrow--up' : 'icon--arrow--down'}></span>
+        </li>
            {isFeaturesOpen && <ul className='features--lists'>
                 <li className='features--lists--todo'>Todo List</li>
                 <li className='features--lists--calendar'>Calendar</li>
                 <li className='features--lists--reminders'>Reminders</li>
                 <li className='features--lists--planning'>Planning</li>
             </ul>}
-        <li className='nav--company' onClick={toggleList} id="company">Company<span className={isCompanyOpen ? 'icon--arrow--up' : 'icon--arrow--down'}></span></li>
-             {isCompanyOpen && <ul>
+        <li  id="company" 
+        className='nav--company' 
+        onClick={toggleList}>
+        Company
+        <span className={isCompanyOpen ? 'icon--arrow--up' : 'icon--arrow--down'}></span>
+        </li>
+             {isCompanyOpen && <ul className='company--lists'>
                 <li>History</li>
                 <li>Our Team</li>
                 <li>Blog</li>
