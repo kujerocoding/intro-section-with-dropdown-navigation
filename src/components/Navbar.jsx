abbf91f8c734b6c6c65c8281b682149084b860f6
herom <jerome.manset22@gmail.com>
2023-02-17 03:53:07 +0800

    add state of isListOpen and toggleList function to enable dropdown menu of specific nav list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
 
 const Navbar = (props) => {
+    const [isListOpen, setIsListOpen] = useState(false)
+
+    function toggleList(){
+        setIsListOpen(prevState => !prevState)
+    }
 
   return (
     <nav>
       <img className="icon--menu--close" src="./src/assets/images/icon-close-menu.svg" alt="X icon" onClick={props.handleClick}/>
       <ul className='nav--lists'>
-        <li className='nav--features'>Features<span className='icon--arrow--up'></span></li>
-            {/* <ul>
+        <li className='nav--features' onClick={toggleList}>Features<span className={isListOpen ? 'icon--arrow--up' : 'icon--arrow--down'}></span></li>
+           {isListOpen && <ul className='features--lists'>
                 <li>Todo List</li>
                 <li>Calendar</li>
                 <li>Reminders</li>
                 <li>Planning</li>
-            </ul> */}
+            </ul>}
         <li>Company</li>
             {/* <ul>
                 <li>History</li>
