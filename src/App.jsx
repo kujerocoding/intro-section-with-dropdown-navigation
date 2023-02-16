69c7c1e4b7ca35c898e28722afc62148e2a60ac2
herom <jerome.manset22@gmail.com>
2023-02-17 02:19:29 +0800

    add css variables and css resest

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
+import './App.css'
 
 const App = () => {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <div>
+      
         <img src="../src/assets/images/logo.svg" alt="Snap logo" />
         <img src="../src/assets/images/icon-menu.svg" alt="Hamburger menu" />
-      </div>
+      
     </div>
   )
 }
