b5160c0bb6eef6b805296f0e894cc788127a5ce6
herom <jerome.manset22@gmail.com>
2023-02-17 07:03:15 +0800

    put the state from App component to Header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,14 @@ import React, { useState } from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
 import './App.css'
-import Navbar from './components/Navbar'
 
 const App = () => {
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
   
-  function handleMenuClick(){
-    setIsMenuOpen(prevState => !prevState)
-  }
   
   return (
     <div className='app--container'>
-      <Header handleClick={handleMenuClick}/>
-      {isMenuOpen && <Navbar handleClick={handleMenuClick}/>}
+      <Header/>
       <Main />
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Navbar from './Navbar'
+import { useState } from 'react'
 
-const Header = (props) => {
+const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+  
+    function handleMenuClick(){
+        setIsMenuOpen(prevState => !prevState)
+    }
+
   return (
     <header>
         <img src="../src/assets/images/logo.svg" alt="Snap logo" />
-        <img className="icon--menu" src="../src/assets/images/icon-menu.svg" alt="Hamburger menu" onClick={props.handleClick} />
+        <img className="icon--menu" src="../src/assets/images/icon-menu.svg" alt="Hamburger menu" onClick={handleMenuClick} />
+        {isMenuOpen && <Navbar handleClick={handleMenuClick}/>}
     </header>
   )
 }
