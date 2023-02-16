7001a0c2335c48f5b7e86d9e1ed3d015647e6cf4
herom <jerome.manset22@gmail.com>
2023-02-17 06:48:15 +0800

    add media query and update styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,18 +3,20 @@ import React from 'react'
 const Main = () => {
   return (
     <div className='main--container'>
-        <img src="./src/assets/images/image-hero-mobile.png" alt="Man holding a laptop" />
+        <div className='main--img--container'>
+        </div>
         <div className='main--text--container'>
-            <h1>Make remote work</h1>
+            <h1>Make <span className='text--remotework'>remote work</span></h1>
             <p>Get your team in sync, no matter your locations. Streamline processes, create team rituals, and watch productivity soar.</p>
             <button>Learn more</button>
+            <div className='main--client--container'>
+                <img className='client--icons ' src="./src/assets/images/client-databiz.svg" alt="Databiz logo" />
+                <img className='client--icons audiophile--icon' src="./src/assets/images/client-audiophile.svg" alt="Audiophile logo" />
+                <img className='client--icons ' src="./src/assets/images/client-meet.svg" alt="Meet logo" />
+                <img className='client--icons ' src="./src/assets/images/client-maker.svg" alt="Maker logo" />
+            </div>
         </div>
-      <div className='main--client--container'>
-        <img className='client--icons ' src="./src/assets/images/client-databiz.svg" alt="Databiz logo" />
-        <img className='client--icons audiophile--icon' src="./src/assets/images/client-audiophile.svg" alt="Audiophile logo" />
-        <img className='client--icons ' src="./src/assets/images/client-meet.svg" alt="Meet logo" />
-        <img className='client--icons ' src="./src/assets/images/client-maker.svg" alt="Maker logo" />
-      </div>
+      
     </div>
   )
 }
