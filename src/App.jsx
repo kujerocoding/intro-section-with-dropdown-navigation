075ae7cb57b188611d12be8fa843f025a592964e
herom <jerome.manset22@gmail.com>
2023-02-17 01:53:18 +0800

    add elements to the Header, Main and Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
 
 const App = () => {
   return (
     <div>
-      app
+      <Header />
+      <Main />
     </div>
   )
 }
