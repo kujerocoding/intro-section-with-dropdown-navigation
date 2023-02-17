3168e21678d143abd84a17a67845b826bc0e42e3
herom <jerome.manset22@gmail.com>
2023-02-17 09:03:13 +0800

    add attribution

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ const App = () => {
     <div className='app--container'>
       <Header/>
       <Main />
+      <div className='attribution'>
+            Challenge by <a href="https://www.frontendmentor.io/challenges" target="_blank">Frontend Mentor.</a> Coded by <a href='https://github.com/kujerocoding' target="_blank">kujerocoding</a>
+      </div>
     </div>
   )
 }
